b14808f3052c9752ecaf3904a182effc6118d2b4
kay <kayshiro@live.no>
2021-10-08 16:13:26 +0200

    API generating

--- app/src/main/java/com/kay/eatsomething/RecipesFragment.kt
+++ app/src/main/java/com/kay/eatsomething/RecipesFragment.kt
@@ -1,18 +1,35 @@
 package com.kay.eatsomething
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kay.eatsomething.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
+
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+
+        showShimmerEffect()
+
+        return binding.root
+    }
+
+    private fun showShimmerEffect() {
+        binding.shimmerFrameLayout.startShimmer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
