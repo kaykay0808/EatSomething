0176045c73c36e389df5a72950f504f870abf161
kay <kayshiro@live.no>
2021-11-18 14:42:03 +0100

    adapter issue

--- app/src/main/java/com/kay/eatsomething/adapters/RecipesAdapters.kt
+++ app/src/main/java/com/kay/eatsomething/adapters/RecipesAdapters.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.kay.eatsomething.databinding.RecipesRowLayoutBinding
 import com.kay.eatsomething.models.FoodRecipe
 import com.kay.eatsomething.models.FoodTypeResult
@@ -13,12 +14,13 @@ class RecipesAdapters : RecyclerView.Adapter<RecipesAdapters.MyViewHolder>() {
 
     private var recipes = emptyList<FoodTypeResult>()
 
-    class MyViewHolder(private val binding: RecipesRowLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    class MyViewHolder(val binding: RecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipesAdapters.MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val inflater = LayoutInflater.from(parent.context)
 
         return RecipesAdapters.MyViewHolder(
@@ -32,15 +34,29 @@ class RecipesAdapters : RecyclerView.Adapter<RecipesAdapters.MyViewHolder>() {
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentRecipe = recipes[position] // <- we might need to change this later.
+        // load image from URL
+        holder.binding.recipeImageView.load(currentRecipe.image) { crossfade(600) }
+        // set number of likes(<3)
+        holder.binding.heartTextView.text = currentRecipe.aggregateLikes.toString()
+        // set number of minutes
+        holder.binding.clockTextView.text = currentRecipe.readyInMinutes.toString()
+        // vegan image?
+        holder.binding.leafImageView
+        // heart image?
+        holder.binding.heartImageView
     }
 
     override fun getItemCount(): Int {
         return recipes.size
     }
 
-    fun setData(newData: FoodRecipe){
-        val recipesDiffUtil = RecipesDiffUtil(recipes, newData.foodTypeResults) // <- the old list is recipe and the new list is newData.
-        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil) // <- This calculate the difference between those two list.
+    fun setData(newData: FoodRecipe) {
+        val recipesDiffUtil = RecipesDiffUtil(
+            recipes,
+            newData.foodTypeResults
+        ) // <- the old list is recipe and the new list is newData.
+        val diffUtilResult =
+            DiffUtil.calculateDiff(recipesDiffUtil) // <- This calculate the difference between those two list.
         recipes = newData.foodTypeResults
         diffUtilResult.dispatchUpdatesTo(this) // refer to this class (RecyclerView adapter)
 
--- app/src/main/java/com/kay/eatsomething/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/kay/eatsomething/ui/fragments/recipes/RecipesFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kay.eatsomething.MainViewModel
 import com.kay.eatsomething.adapters.RecipesAdapters
@@ -18,10 +18,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
 
-    private lateinit var mainViewModel: MainViewModel
-    private val mAdapter by lazy { RecipesAdapters() }
     private var _binding: FragmentRecipesBinding? = null
     private val binding get() = _binding!!
+
+    //private lateinit var mainViewModel: MainViewModel
+    private val mainViewModel: MainViewModel by viewModels()
+    private val mAdapter by lazy { RecipesAdapters() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,10 +32,11 @@ class RecipesFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentRecipesBinding.inflate(inflater, container, false)
 
-        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        //mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+
 
         setupRecyclerView()
-        //showShimmerEffect()
+        showShimmerEffect()
         requestApiData()
 
         return binding.root
